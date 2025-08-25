52b315967584d9570f63f74ac826959a4bbb703e
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-08-25 18:07:12 +0200

    add comments to ContactDetailsPage

--- src/pages/ContactDetailsPage.jsx
+++ src/pages/ContactDetailsPage.jsx
@@ -1,19 +1,25 @@
 import { useParams } from "react-router-dom";
+//import array dei partecipanti
 import { participants } from "../data/participants";
 
 export default function ContactDetailsPage() {
+  //useParams for dynamic params
   const { participantId } = useParams();
+  //find del singolo contatto nell'array dei partecipanti al viaggio che si è selezionato sulla base dell'id del partecipante
   const contact = participants.find((p) => p.id === parseInt(participantId));
 
   if (!contact) {
+    //fallback in caso di id non presente in array al momento
     return <h2>Partecipante non trovato</h2>;
   }
 
   return (
+    //container base per i dettagli di contatto
     <div className="container p-5 d-flex align-items-center flex-column">
       <div className="pageTitle">
         <h2 className="p-3">Dettagli di Contatto</h2>
       </div>
+      {/* lista con dati del partecipante*/}
       <ul className="list-unstyled d-flex flex-column gap-3 ">
         <li>
           <strong>Nome:</strong> {contact.nome}
