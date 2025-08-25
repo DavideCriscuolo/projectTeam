eb6a99be2c3bf46bb1bce5cb1de842018b3844cb
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-08-25 17:15:41 +0200

    aggiunto: - Default Layout al progetto -  creato il componente Header - creato il componente Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DefaultLayout from "./layout/DefaultLayout";
+import HomePage from "./pages/HomePage";
+
+
 function App() {
-  return <></>;
+
+  return (
+    <>
+
+      <BrowserRouter>
+
+        <Routes>
+          <Route Component={DefaultLayout}>
+            <Route path="/" Component={HomePage} />
+
+          </Route>
+        </Routes>
+
+      </BrowserRouter>
+
+
+    </>
+  );
 }
 
 export default App;
