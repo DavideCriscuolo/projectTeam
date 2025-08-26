f27f36c11b66af6cbb3b50e694a0b12e463d1c3f
DavideCriscuolo <davide.alcatel@gmail.com>
2025-08-26 11:26:28 +0200

    aggiunto link alla card per pagina dettaglio viaggio

--- src/components/ListaViaggi.jsx
+++ src/components/ListaViaggi.jsx
@@ -1,33 +1,36 @@
 import { useState } from "react";
 import "./../css/ListaViaggi.css";
+import { Link } from "react-router-dom";
 export default function ListaViaggi({ viaggi }) {
   const [isHover, setIsHover] = useState(null);
   return (
     <>
       <div className="container border  border-primary">
         <h1> Card Viaggi </h1>
-        <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2">
+        <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2  row-cols-lg-3">
           {viaggi?.map((trip) => (
             <div key={trip.id} className="col p-3">
-              <div className="card text-bg-dark">
-                <img
-                  src={trip.img}
-                  className="card-img h-100"
-                  alt="Img copertina viaggio"
-                />
+              <Link to={`/trip/${trip.id}`}>
+                <div className="card text-bg-dark">
+                  <img
+                    src={trip.img}
+                    className="card-img h-100"
+                    alt="Img copertina viaggio"
+                  />
 
-                <>
-                  <div
-                    className={`card-img-overlay d-flex justify-content-center align-items-center ${
-                      isHover === trip.id ? "overlay" : "hide"
-                    }`}
-                    onMouseEnter={() => setIsHover(trip.id)}
-                    onMouseLeave={() => setIsHover(null)}
-                  >
-                    <h5 className="card-title">{trip.nomeViaggio}</h5>
-                  </div>
-                </>
-              </div>
+                  <>
+                    <div
+                      className={`card-img-overlay d-flex justify-content-center align-items-center ${
+                        isHover === trip.id ? "overlay" : "hide"
+                      }`}
+                      onMouseEnter={() => setIsHover(trip.id)}
+                      onMouseLeave={() => setIsHover(null)}
+                    >
+                      <h5 className="card-title">{trip.nomeViaggio}</h5>
+                    </div>
+                  </>
+                </div>
+              </Link>
             </div>
           ))}
         </div>
