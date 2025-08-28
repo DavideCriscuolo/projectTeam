dc240872f70d3d91d48e2a39b0018ecfdb381019
DavideCriscuolo <davide.alcatel@gmail.com>
2025-08-28 12:19:05 +0200

    cambiato colore ai link di navigzione nel header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,12 +33,12 @@ export default function Header() {
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <NavLink className="nav-link text-primary " to="/">
+              <NavLink className="nav-link " to="/">
                 <strong>Home</strong>
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link text-primary" to="/features">
+              <NavLink className="nav-link " to="/features">
                 <strong>Features</strong>
               </NavLink>
             </li>
